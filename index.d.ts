2d210b72105805afa6bb03d602eac1e5a5b96b42
Justin Wolcott <Justin.W.Wolcott@gmail.com>
2024-01-29 09:35:56 -0600

    update to ts defs

--- index.d.ts
+++ index.d.ts
@@ -1,24 +1,59 @@
-type Base64String = string;
+// index.d.ts
 
-type ScanReturnType = {
-  ciphertext: Base64String;
-  salt: Base64String;
-  iv: Base64String;
-  publicEncryptionKey: Base64String;
-  apiCiphertextSignature: Base64String;
-  clientCipertextSignature: Base64String;
-};
-
-type EventType = "access" | "visits" | "signup" | "login";
-
-declare class XRAY {
-  public scan(
-    eventType: EventType,
-    userId: string,
-    publicEncryptionKey: Base64String,
-    timeout?: number,
-    mode?: "safe"
-  ): Promise<ScanReturnType>;
+/**
+ * Options for the `scan` method of XRAY class.
+ */
+export interface ScanOptions {
+  /**
+   * The API URL to be used in the scan process.
+   */
+  apiUrl: string;
 }
 
-export { XRAY };
+/**
+* Result from the `scan` method of XRAY class.
+*/
+export interface ScanResult {
+  /**
+   * The encrypted payload, encoded in base64.
+   */
+  encryptedB64Payload: string;
+}
+
+/**
+* Class representing the XRAY functionality.
+* This class handles the loading and instantiation of the XRAY object.
+*/
+export class XRAY {
+  /**
+   * The source URL for the iframe, if provided.
+   * @private
+   */
+  private iframeSrc?: string;
+
+  /**
+   * Constructor for the XRAY class.
+   * @param iframeSrc - Optional source URL for the iframe.
+   */
+  constructor(iframeSrc?: string);
+
+  /**
+   * Loads the necessary components for the XRAY functionality.
+   * This method dynamically loads and decodes a base64-encoded string
+   * representing the library, and initializes an instance of XRAY.
+   * @returns A promise that resolves when the loading is complete.
+   */
+  load(): Promise<void>;
+
+  /**
+   * Instance of XRAY, created after `load` method is called.
+   */
+  xray?: XRAY;
+
+  /**
+   * Scans and processes data based on provided options.
+   * @param options - The options for the scan, including API URL.
+   * @returns A promise that resolves to the scan result.
+   */
+  scan(options: ScanOptions): Promise<ScanResult>;
+}
